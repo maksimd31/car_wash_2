5791959b25a4e99fe0ed8ca425da850845e50f02
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-10-16 21:59:56 +0300

    start v3

--- crm_car_wash/website/views.py
+++ crm_car_wash/website/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+# Импортируем аутентификацию
+from django.contrib.auth import authenticate, login, logout
+# импортируем со0бщения
+from django.contrib import messages
+
 
 # Create your views here.
+
+def home(request):
+    """Отрисуем главную страницу"""
+    return render(request, 'home.html', {})
+
+
+def login_user(request):
+    """фукция входа"""
+    pass
+
+
+def logout_user(request):
+    """Функция выхода"""
+    pass

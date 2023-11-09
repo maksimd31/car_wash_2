4024252efa4254b98016b1041f458f4e6d4997ae
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-11-09 22:27:36 +0300

    version project v1.4

--- crm_car_wash/crm_car_wash/settings.py
+++ crm_car_wash/crm_car_wash/settings.py
@@ -15,6 +15,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+CSRF_TRUSTED_ORIGINS=["http://crmcarwash.pythonanywhere.com/"]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

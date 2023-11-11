239bf2156b369c5249a60c7377d89aded866583d
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-11-11 17:10:02 +0300

    version project v1.5

--- crm_car_wash/website2/forms.py
+++ crm_car_wash/website2/forms.py
@@ -1,9 +1,8 @@
-from django import forms
-from .models import Client, Order, Employee
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Client
+from .models import Client, Order, Employee
 
 
 # filename forms.py
@@ -39,11 +38,6 @@ class EmployeeForm(forms.ModelForm):
 
 # От сюда
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django import forms
-from .models import Client
-
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",
@@ -60,29 +54,23 @@ class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
-        self.fields['full_name'].widget.attrs['class'] = 'form-control'
-        self.fields['full_name'].widget.attrs['placeholder'] = 'Full Name'
-        self.fields['full_name'].label = ''
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
         self.fields[
-            'full_name'].help_text = '<span class="form-text text-muted"><small> Требуется. 150 символов или меньше. ' \
-                                     'Только буквы, цифры и @/./+/-/_. </small></span>'
+            'username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
         self.fields['password1'].label = ''
         self.fields[
-            'password1'].help_text = '<ul class="form-text text-muted small"><li>Ваш пароль\\\\t не должен быть слишком ' \
-                                     'похож на другую вашу личную информацию.</li><li>Ваш пароль должен содержать ' \
-                                     'не менее 8 символов.</li><li>Ваш пароль не может быть часто ' \
-                                     'используемым паролем. ' \
-                                     '</li><li>Ваш пароль не может быть полностью цифровым.</li></ul>'
+            'password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields[
-            'password2'].help_text = '<span class="form-text text-muted"><small>Введите тот же пароль, что и раньше, ' \
-                                     'для проверки.</small></span>'
+            'password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
 
 class AddRecordForm(forms.ModelForm):

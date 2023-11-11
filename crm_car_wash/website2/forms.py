e57eca4d93573408256cdbb1c618ff50c8d34eb7
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-11-11 17:25:19 +0300

    version project v1.5

--- crm_car_wash/website2/forms.py
+++ crm_car_wash/website2/forms.py
@@ -41,11 +41,11 @@ class EmployeeForm(forms.ModelForm):
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",
-                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     first_name = forms.CharField(label="", max_length=100,
-                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
     last_name = forms.CharField(label="", max_length=100,
-                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
 
     class Meta:
         model = User
@@ -55,22 +55,22 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].widget.attrs['placeholder'] = 'Имя пользователя'
         self.fields['username'].label = ''
         self.fields[
-            'username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+            'username'].help_text = '<span class="form-text text-muted"><small>Требуется. 150 символов или меньше. Только буквы, цифры и @/./+/-/_.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Пароль'
         self.fields['password1'].label = ''
         self.fields[
-            'password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+            'password1'].help_text = '<ul class="form-text text-muted small"><li>Ваш пароль не должен быть слишком похож на другую вашу личную информацию.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Повторение пароля'
         self.fields['password2'].label = ''
         self.fields[
-            'password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+            'password2'].help_text = '<span class="form-text text-muted"><small>Введите тот же пароль, что и раньше, для проверки.</small></span>'
 
 
 class AddRecordForm(forms.ModelForm):

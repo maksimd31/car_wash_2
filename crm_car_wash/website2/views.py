a826deda55462ee4866a1aee2cc40f005377fbb1
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-11-12 11:42:40 +0300

    version project v1.5.3

--- crm_car_wash/website2/views.py
+++ crm_car_wash/website2/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.core import management
 
-
 # CLIENT
 # filename views.py
 # def add_client(request):
@@ -31,116 +30,116 @@ from django.core import management
 
 
 # filename views.py
-def delete_client(request):
-    """
-    Удаление пользователей
-    :param request: ответ на запрос GET
-    :return: render(request, 'delete_client.html')
-    """
-    if request.method == 'POST':
-        license_plate = request.POST.get('license_plate')
-        try:
-            client = Client.objects.get(license_plate=license_plate)
-            client.delete()
-            return redirect('client_list')
-        except Client.DoesNotExist:
-            error_message = 'Клиент с указанным государственным номером не найден.'
-            return render(request, 'delete_client.html', {'error_message': error_message})
-
-    return render(request, 'delete_client.html')
+# def delete_client(request):
+#     """
+#     Удаление пользователей
+#     :param request: ответ на запрос GET
+#     :return: render(request, 'delete_client.html')
+#     """
+#     if request.method == 'POST':
+#         license_plate = request.POST.get('license_plate')
+#         try:
+#             client = Client.objects.get(license_plate=license_plate)
+#             client.delete()
+#             return redirect('client_list')
+#         except Client.DoesNotExist:
+#             error_message = 'Клиент с указанным государственным номером не найден.'
+#             return render(request, 'delete_client.html', {'error_message': error_message})
+#
+#     return render(request, 'delete_client.html')
 
 
 # filename views.py
-def update_client(request, license_plate):
-    """
-    Редактирование Пользователей
-    :param request: ответ на запрос GET
-    :param license_plate:
-    :return: render(request, 'update_client.html'
-    """
-    client = get_object_or_404(Client, license_plate=license_plate)
-
-    if request.method == 'POST':
-        form = ClientUpdateForm(request.POST, instance=client)
-        if form.is_valid():
-            form.save()
-            return redirect('client_detail', license_plate=license_plate)
-    else:
-        form = ClientUpdateForm(instance=client)
-
-    return render(request, 'update_client.html', {'form': form, 'client': client})
+# def update_client(request, license_plate):
+#     """
+#     Редактирование Пользователей
+#     :param request: ответ на запрос GET
+#     :param license_plate:
+#     :return: render(request, 'update_client.html'
+#     """
+#     client = get_object_or_404(Client, license_plate=license_plate)
+#
+#     if request.method == 'POST':
+#         form = ClientUpdateForm(request.POST, instance=client)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('client_detail', license_plate=license_plate)
+#     else:
+#         form = ClientUpdateForm(instance=client)
+#
+#     return render(request, 'update_client.html', {'form': form, 'client': client})
 
 
 # ORDER
 # filename views.py
-def create_order(request):
-    """
-    Создание заказа, сделки
-    :param request: ответ на запрос GET
-    :return: return render(request, 'create_order.html'
-    """
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('order_list')
-    else:
-        form = OrderForm()
-    return render(request, 'create_order.html', {'form': form})
+# def create_order(request):
+#     """
+#     Создание заказа, сделки
+#     :param request: ответ на запрос GET
+#     :return: return render(request, 'create_order.html'
+#     """
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('order_list')
+#     else:
+#         form = OrderForm()
+#     return render(request, 'create_order.html', {'form': form})
 
 
 # filename views.py
-def delete_order(request, order_id):
-    """
-    Удаление заказа/сделки
-    :param request: ответ на запрос GET
-    :param order_id: идентификатор заказа/сделки
-    :return: render(request, 'delete_order.html'
-    """
-    order = get_object_or_404(Order, pk=order_id)
-    # Здесь мы используем функцию get_object_or_404,
-    # чтобы получить объект модели Order по его первичному ключу (pk).
-    # Если заказ с указанным order_id не существует, будет сгенерирована страница ошибки HTTP 404.
-    if request.method == 'POST':
-        order.delete()
-        return redirect('order_list')
-    #     Здесь мы проверяем, был ли отправлен POST-запрос.
-    #     Если это так, то мы удаляем заказ, вызывая метод delete() для объекта order.
-    #     Затем мы перенаправляем пользователя на страницу, которая отображает список заказов
-    #     (в данном случае с именем order_list).
-
-    return render(request, 'delete_order.html', {'order': order})
+# def delete_order(request, order_id):
+#     """
+#     Удаление заказа/сделки
+#     :param request: ответ на запрос GET
+#     :param order_id: идентификатор заказа/сделки
+#     :return: render(request, 'delete_order.html'
+#     """
+#     order = get_object_or_404(Order, pk=order_id)
+#     # Здесь мы используем функцию get_object_or_404,
+#     # чтобы получить объект модели Order по его первичному ключу (pk).
+#     # Если заказ с указанным order_id не существует, будет сгенерирована страница ошибки HTTP 404.
+#     if request.method == 'POST':
+#         order.delete()
+#         return redirect('order_list')
+#     #     Здесь мы проверяем, был ли отправлен POST-запрос.
+#     #     Если это так, то мы удаляем заказ, вызывая метод delete() для объекта order.
+#     #     Затем мы перенаправляем пользователя на страницу, которая отображает список заказов
+#     #     (в данном случае с именем order_list).
+#
+#     return render(request, 'delete_order.html', {'order': order})
 
 
 # filename views.py
-def update_order(request, order_id):
-    """
-    Редактирование заказа/сделки
-    :param request: ответ на запрос GET
-    :param order_id: идентификатор заказа/сделки
-    :return: render(request, 'update_order.html'
-    """
-    order = get_object_or_404(Order, pk=order_id)
+# def update_order(request, order_id):
+#     """
+#     Редактирование заказа/сделки
+#     :param request: ответ на запрос GET
+#     :param order_id: идентификатор заказа/сделки
+#     :return: render(request, 'update_order.html'
+#     """
+#     order = get_object_or_404(Order, pk=order_id)
+#
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST, instance=order)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('order_list')
+#     else:
+#         form = OrderForm(instance=order)
+#
+#     return render(request, 'update_order.html', {'form': form, 'order': order})
 
-    if request.method == 'POST':
-        form = OrderForm(request.POST, instance=order)
-        if form.is_valid():
-            form.save()
-            return redirect('order_list')
-    else:
-        form = OrderForm(instance=order)
 
-    return render(request, 'update_order.html', {'form': form, 'order': order})
-
-
-def order_list(request):
-    """
-    отображает список заказов (в данном случае с именем order_list).
-    :param request:
-    :return:  render(request, 'order_list.html'
-    """
-    orders = Order.objects.all()
-    return render(request, 'order_list.html', {'orders': orders})
+# def order_list(request):
+#     """
+#     отображает список заказов (в данном случае с именем order_list).
+#     :param request:
+#     :return:  render(request, 'order_list.html'
+#     """
+#     orders = Order.objects.all()
+#     return render(request, 'order_list.html', {'orders': orders})
 
 
 """
@@ -177,43 +176,43 @@ def page_not_found(reqwest, exception):
 
 # employee
 # filename views.py
-def add_employee(request):
-    """
-    Создание сотрудника
-    :param request: ответ на запрос GET или POST
-    :return: render 'add_employee.html' или перенаправление на страницу со списком сотрудников
-    """
-    if request.method == 'POST':
-        form = EmployeeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('employee_list')  # Перенаправление на страницу со списком сотрудников
-    else:
-        form = EmployeeForm()
-
-    context = {'form': form}
-    return render(request, 'add_employee.html', context)
-
-
-def employee_list(request):
-    """
-    Отображение списка сотрудников
-    :param request: ответ на запрос GET
-    :return: render 'employee_list.html' с контекстом, содержащим список сотрудников
-    """
-    employees = Employee.objects.all()
-    context = {'employees': employees}
-    return render(request, 'employee_list.html', context)
+# def add_employee(request):
+#     """
+#     Создание сотрудника
+#     :param request: ответ на запрос GET или POST
+#     :return: render 'add_employee.html' или перенаправление на страницу со списком сотрудников
+#     """
+#     if request.method == 'POST':
+#         form = EmployeeForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('employee_list')  # Перенаправление на страницу со списком сотрудников
+#     else:
+#         form = EmployeeForm()
+#
+#     context = {'form': form}
+#     return render(request, 'add_employee.html', context)
 
 
-def delete_employee(request, employee_id):
-    employee = Employee.objects.get(id=employee_id)
+# def employee_list(request):
+#     """
+#     Отображение списка сотрудников
+#     :param request: ответ на запрос GET
+#     :return: render 'employee_list.html' с контекстом, содержащим список сотрудников
+#     """
+#     employees = Employee.objects.all()
+#     context = {'employees': employees}
+#     return render(request, 'employee_list.html', context)
 
-    if request.method == 'POST':
-        employee.delete()
-        return redirect('employee_list')
-
-    return render(request, 'delete_employee.html', {'employee': employee})
+#
+# def delete_employee(request, employee_id):
+#     employee = Employee.objects.get(id=employee_id)
+#
+#     if request.method == 'POST':
+#         employee.delete()
+#         return redirect('employee_list')
+#
+#     return render(request, 'delete_employee.html', {'employee': employee})
 
 
 # # filename views.py
@@ -226,19 +225,19 @@ def delete_employee(request, employee_id):
 # from .models import Employee
 # from .forms import EmployeeForm
 
-
-def edit_employee(request, employee_id):
-    employee = get_object_or_404(Employee, id=employee_id)
-    if request.method == 'POST':
-        form = EmployeeForm(request.POST, instance=employee)
-        if form.is_valid():
-            form.save()
-            return redirect('employee_list')
-    else:
-        form = EmployeeForm(instance=employee)
-
-    return render(request, 'edit_employee.html', {'form': form})
-
+#
+# def edit_employee(request, employee_id):
+#     employee = get_object_or_404(Employee, id=employee_id)
+#     if request.method == 'POST':
+#         form = EmployeeForm(request.POST, instance=employee)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('employee_list')
+#     else:
+#         form = EmployeeForm(instance=employee)
+#
+#     return render(request, 'edit_employee.html', {'form': form})
+#
 
 # Вот от сюда NEW
 
@@ -328,6 +327,7 @@ def update_client(request, client_id):
     return render(request, 'update_client.html', {'form': form})
 
 
+@authenticated_user_required
 def add_random_client(request):
     """
     Вызывает функцию из management/commands которая рандомно добавляет нового клиента
@@ -339,11 +339,13 @@ def add_random_client(request):
     return redirect('client_home')
 
 
+@authenticated_user_required
 def delete_random_client(request):
     management.call_command('delete_random_client')
     return redirect('client_home')
 
 
+@authenticated_user_required
 def update_random_client(request):
     management.call_command('random_update_client')
     return redirect('client_home')

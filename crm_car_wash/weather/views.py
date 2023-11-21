df3b424a4539e336272f140a67a6738da9010f49
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-11-21 17:57:58 +0300

    version project v1.7

--- crm_car_wash/weather/views.py
+++ crm_car_wash/weather/views.py
@@ -10,36 +10,9 @@ from .forms import CityForm
 
 
 @authenticated_user_required
-# def index(request):
-#     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c56398aa9dd4ea5e0854302e39acf5a5'
-#     city = 'London'
-#
-#     if request.method == 'POST':
-#         form = CityForm(request.POST)
-#         form.save()
-#
-#     form = CityForm()
-#
-#     cities = City.objects.all()
-#
-#     weather_data = []
-#
-#     for city in cities:
-#         r = requests.get(url.format(city)).json()
-#
-#         city_weather = {
-#             'city': city.name,
-#             'temperature': r['main']['temp'],
-#             'description': r['weather'][0]['description'],
-#             'icon': r['weather'][0]['icon'],
-#         }
-#
-#         weather_data.append(city_weather)
-#     context = {'weather_data': weather_data, 'form': form}
-#     return render(request, 'weather.html', context)
-
 def index(request):
-    url = '<http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c56398aa9dd4ea5e0854302e39acf5a5>'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c56398aa9dd4ea5e0854302e39acf5a5'
+    city = 'London'
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -52,7 +25,7 @@ def index(request):
     weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city.name)).json()
+        r = requests.get(url.format(city)).json()
 
         city_weather = {
             'city': city.name,
@@ -62,7 +35,6 @@ def index(request):
         }
 
         weather_data.append(city_weather)
-
     context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather.html', context)
 

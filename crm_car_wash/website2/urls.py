c4fdc9d0d59e48b70666a56f32759b7e9e37b33a
imacson <maksimdis31@gmail.com>
2023-12-11 19:16:23 +0300

    version project v1.14 working weather.html v1.6

--- crm_car_wash/website2/urls.py
+++ crm_car_wash/website2/urls.py
@@ -44,9 +44,11 @@ urlpatterns = [
     path('delete_random_client/', delete_random_client, name='delete_random_client'),
     path('update_random_client/', update_random_client, name='update_random_client'),
     path('weather_u/', include(weather_u)),
-    path('weather', views.weather_view, name='weather'),
-    path('delete_weather/', views.delete_weather, name='delete_weather'),
-    path('save_weather/', views.save_weather, name='save_weather'),
+    path('weather', views.get_weather, name='weather'),
+
+    # path('weather', views.weather_view, name='weather'),
+    # path('delete_weather/', views.delete_weather, name='delete_weather'),
+    # path('save_weather/', views.save_weather, name='save_weather'),
     # path('index/', views.index, name='index'),
     path('new_order', views.new_order, name='new_order'),
 

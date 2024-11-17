d18d3894473c77faf277c24d0bc99b9b6f47b35e
M B <mb@MacBook-Pro-M.local>
2024-11-17 18:51:23 +0300

    new_startapp

--- crm_car_wash/registr_time/models.py
+++ crm_car_wash/registr_time/models.py
@@ -1,17 +1,15 @@
+import time  # Добавьте этот импорт
 
-
-
-from django.db import models
-from django.utils import timezone
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 class Timer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.FloatField(null=True, blank=True)
     elapsed_time = models.FloatField(default=0)
-    intervals = models.JSONField(default=list)  # Для хранения промежуточных интервалов
+    intervals = models.JSONField(default=list)
 
     def __str__(self):
         return f'Timer for {self.user.username}'

61dd12c23ea014d52cdf5ae5d7901eb0ae1fd726
M B <mb@MacBook-Pro-M.local>
2024-11-17 21:22:10 +0300

    new_startapp

--- crm_car_wash/new_time_register/urls.py
+++ crm_car_wash/new_time_register/urls.py
@@ -1,7 +1,10 @@
 # from .views import add_random_client, delete_random_client, update_random_client
 
 from django.urls import path
+
 from crm_car_wash.new_time_register import views
+
+# from crm_car_wash.new_time_register import views
 
 urlpatterns = [
     path('home/', views.home_registr, name='home_registr'),

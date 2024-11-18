79a6187992cfb3b6faba621c62b73b13f081680a
M B <mb@MacBook-Pro-M.local>
2024-11-18 12:01:28 +0300

    new_startapp

--- crm_car_wash/new_time_register/urls.py
+++ crm_car_wash/new_time_register/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('register/', views.register_user, name='register_reg'),
     path('logout/', views.logout_user, name='logout'),
     # path('tim/', views.timer_view, name='tim'),
-    path('timer/', views.time_interval_view, name='time_interval'),
+    # path('timer/', views.time_interval_view, name='time_interval'),
+    path('timer/', views.time_interval_view, name='time_interval_view'),  # Убедитесь, что здесь правильный путь
 
 ]
 

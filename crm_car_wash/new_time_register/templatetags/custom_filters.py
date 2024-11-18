8d82b67e15a50af1ee818eaa25ac6642b849b148
M B <mb@MacBook-Pro-M.local>
2024-11-19 00:13:36 +0300

    new_startapp_n2

--- crm_car_wash/new_time_register/templatetags/custom_filters.py
+++ crm_car_wash/new_time_register/templatetags/custom_filters.py
@@ -3,10 +3,10 @@ from django import template
 register = template.Library()
 
 @register.filter
-def duration_format(value):
-    if value:
-        total_seconds = int(value.total_seconds())
+def duration_format(duration):
+    if duration:
+        total_seconds = int(duration.total_seconds())
         hours, remainder = divmod(total_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
-        return f"{hours:02}:{minutes:02}:{seconds:02}"
+        return f"{hours} ч {minutes} мин {seconds} сек"
     return "00:00:00"

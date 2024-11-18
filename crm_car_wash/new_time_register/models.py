218a0d7842560c3f41505428fc7e5fb2e260fe1b
M B <mb@MacBook-Pro-M.local>
2024-11-18 12:35:16 +0300

    new_startapp

--- crm_car_wash/new_time_register/models.py
+++ crm_car_wash/new_time_register/models.py
@@ -18,7 +18,7 @@
 #
 #     def __str__(self):
 #         return f'Timer for {self.user.username}'
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.db import models
 from django.contrib.auth.models import User

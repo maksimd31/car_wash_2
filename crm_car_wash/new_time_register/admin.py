907edc68f1aaf4831553e4ac7549e7de11cf2347
M B <mb@MacBook-Pro-M.local>
2024-11-18 14:41:50 +0300

    new_startapp

--- crm_car_wash/new_time_register/admin.py
+++ crm_car_wash/new_time_register/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import TimeInterval, DailySummary
+
+admin.site.register(TimeInterval)
+admin.site.register(DailySummary)

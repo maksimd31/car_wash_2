dc0eff951a06b33296f292e831438f55d3db6b15
M B <mb@MacBook-Pro-M.local>
2024-11-19 16:59:23 +0300

    working time_interval_view

--- crm_car_wash/new_time_register/admin.py
+++ crm_car_wash/new_time_register/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import TimeInterval
+from .models import TimeInterval, DailySummary
 
 admin.site.register(TimeInterval)
+admin.site.register(DailySummary)

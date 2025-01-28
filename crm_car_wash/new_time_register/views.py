b47712b03fa0b3d657d3c2f921305b9c2939581b
mb <maksimdis31@gmail.com>
2025-01-28 17:43:03 +0300

    fun server 19

--- crm_car_wash/new_time_register/views.py
+++ crm_car_wash/new_time_register/views.py
@@ -358,6 +358,53 @@ def add_manual_interval(user, start_time_str, end_time_str):
 #         'daily_summaries': daily_summaries,
 #     })
 
+# @authenticated_user_required
+# def time_interval_view(request):
+#     moscow_tz = pytz.timezone('Europe/Moscow')
+#
+#     # Получение текущей даты или даты из GET-запроса
+#     selected_date_str = request.GET.get('date', timezone.now().date())
+#
+#     # Проверяем, является ли selected_date_str строкой
+#     if isinstance(selected_date_str, str):
+#         selected_date = datetime.strptime(selected_date_str, '%Y-%m-%d').date()
+#     else:
+#         selected_date = selected_date_str  # Если это уже дата, используем её
+#
+#     if request.method == 'POST':
+#         if 'start' in request.POST:
+#             return handle_start_interval(request, moscow_tz)
+#         elif 'stop' in request.POST:
+#             return handle_stop_interval(request, moscow_tz)
+#         elif 'reset' in request.POST:
+#             return handle_reset_intervals(request)
+#         elif 'delete_summary' in request.POST:
+#             return handle_delete_summary(request)
+#         elif 'add_manual_interval' in request.POST:
+#             return handle_add_manual_interval(request)
+#
+#     intervals = TimeInterval.objects.filter(user=request.user, date_create__date=selected_date)
+#     formatted_intervals, total_duration = format_intervals(intervals)
+#     update_daily_summary(request.user, intervals, total_duration)
+#
+#     daily_summaries = DailySummary.objects.filter(user=request.user).order_by('date')
+#
+#     #
+#     # intervals = TimeInterval.objects.filter(user=request.user, date_create__date=selected_date)
+#     # formatted_intervals, total_duration = format_intervals(intervals)
+#     # update_daily_summary(request.user, intervals, total_duration)
+#     # daily_summaries = DailySummary.objects.filter(user=request.user).order_by('date')
+#
+#
+#
+#     return render(request, 'time_interval.html', {
+#         'formatted_intervals': formatted_intervals,
+#         'daily_summaries': daily_summaries,
+#         'selected_date': selected_date,
+#         'intervals': intervals,
+#     })
+
+
 @authenticated_user_required
 def time_interval_view(request):
     moscow_tz = pytz.timezone('Europe/Moscow')
@@ -370,6 +417,20 @@ def time_interval_view(request):
         selected_date = datetime.strptime(selected_date_str, '%Y-%m-%d').date()
     else:
         selected_date = selected_date_str  # Если это уже дата, используем её
+
+    # Получаем текущую дату и время
+    now = timezone.now()
+
+    # Проверяем наличие активного интервала
+    active_intervals = TimeInterval.objects.filter(user=request.user, end_time__isnull=True)
+
+    # Если есть активный интервал и он начался до текущей даты
+    if active_intervals.exists():
+        active_interval = active_intervals.first()  # Предполагаем, что только один активный интервал
+        if active_interval.start_time.date() < now.date():
+            # Завершаем интервал
+            active_interval.end_time = now
+            active_interval.save()
 
     if request.method == 'POST':
         if 'start' in request.POST:
@@ -389,20 +450,14 @@ def time_interval_view(request):
 
     daily_summaries = DailySummary.objects.filter(user=request.user).order_by('date')
 
-    #
-    # intervals = TimeInterval.objects.filter(user=request.user, date_create__date=selected_date)
-    # formatted_intervals, total_duration = format_intervals(intervals)
-    # update_daily_summary(request.user, intervals, total_duration)
-    # daily_summaries = DailySummary.objects.filter(user=request.user).order_by('date')
-
-
-
     return render(request, 'time_interval.html', {
         'formatted_intervals': formatted_intervals,
         'daily_summaries': daily_summaries,
         'selected_date': selected_date,
         'intervals': intervals,
     })
+
+
 
 def handle_start_interval(request, moscow_tz):
     active_interval = TimeInterval.objects.filter(user=request.user, end_time__isnull=True).first()
